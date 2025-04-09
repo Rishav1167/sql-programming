2383f7d9f761e9288b2f6d596499561b2e9a3e7b
Rishav1167 <Rishav1167>
2025-04-09 15:38:39 +0530

    [Rishav] Add : UC 2 - Ability to create a employee payrol table in the payroll service database

--- Employee_Payroll/queries.sql
+++ Employee_Payroll/queries.sql
@@ -4,3 +4,13 @@ CREATE DATABASE payroll_services;
 show databases ;
 
 use payroll_services
+
+
+-- # UC 2 - Ability to create a employee payroll table in the payroll service database
+create table employee_payroll(
+    id INT unsigned NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50),
+    salary DOUBLE,
+    start_date DATE,
+    PRIMARY KEY (id)
+)
