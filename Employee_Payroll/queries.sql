39d9ef06ec5ee725b2a8616c81e6f88f5b399f2d
Rishav1167 <Rishav1167>
2025-04-09 15:44:26 +0530

    [Rishav] Add : UC 3 - Ability to create employee payroll data in the payroll service database

--- Employee_Payroll/queries.sql
+++ Employee_Payroll/queries.sql
@@ -14,3 +14,10 @@ create table employee_payroll(
     start_date DATE,
     PRIMARY KEY (id)
 )
+
+-- # UC 3 - Ability to create employee payroll data in the payroll service database
+INSERT INTO employee_payroll
+values
+    (1,'Rishav',150000.0,'2003-1-29'),
+    (2,'Anmol',120000.0,'2003-10-9'),
+    (3,'Ankit',100000.0,'2002-7-2');
