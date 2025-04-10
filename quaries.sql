88513d32664da13a67df5bb27ba0ed12a4c81a4b
Rishav1167 <Rishav1167>
2025-04-10 11:05:25 +0530

    [Rishav] Add : UC2 - create a Table with first and last names, address, city, state, zip, phone number and email as its attributes

--- quaries.sql
+++ quaries.sql
@@ -2,3 +2,17 @@
 create database address_book;
 
 use address_book;
+
+-- # UC2 - create a Table with first and last names, address, city, state, zip, phone number and email as its attributes
+CREATE TABLE address_book (
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    address VARCHAR(100),
+    city VARCHAR(50),
+    state VARCHAR(20),
+    zip VARCHAR(6),
+    phone VARCHAR(10),
+    email VARCHAR(50)
+);
+
+
